912e87b987924cb7613bc9d605c21f90053af305
zafar-m3n <thezafar.m3n@gmail.com>
2025-01-11 22:49:18 +0530

    Sticky header

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -5,20 +5,19 @@ import Squares from "@/components/animated/Squares";
 
 const DefaultLayout = () => {
   return (
-    <div className="relative flex flex-col min-h-screen bg-[#010101] text-[#f2f2f2] overflow-hidden">
-      <header className="sticky top-0 z-50 bg-transparent">
+    <div className="relative flex flex-col min-h-screen bg-[#010101] text-[#f2f2f2]">
+      <header className="sticky top-0 z-50 bg-[#010101]">
         <Header />
       </header>
       <main className="relative flex-grow">
-        <div className="absolute inset-0">
+        <div className="absolute inset-0 pointer-events-none">
           <Squares speed={0.5} squareSize={50} direction="up" borderColor="#0bf40a33" hoverFillColor="#131313" />
         </div>
-
         <div className="relative z-10 p-4">
           <Outlet />
         </div>
       </main>
-      <footer className="relative z-10 p-4 text-center">Footer goes here</footer>
+      <footer className="relative z-10 p-4 text-center bg-[#010101]">Footer goes here</footer>
     </div>
   );
 };
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div>Home Page</div>;
 };
 
 export default HomePage;
