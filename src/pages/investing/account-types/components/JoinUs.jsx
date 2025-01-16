7e4111293d9b1c3d2d5ae028501573e8d3c75632
zafar-m3n <thezafar.m3n@gmail.com>
2025-01-16 18:52:43 +0530

    Account Types

---
+++ src/pages/investing/account-types/components/JoinUs.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Icon from "@/components/ui/Icon";
+import Vortex from "@/components/animated/Vortex";
+
+function JoinUs() {
+  return (
+    <div className="container mx-auto rounded-md h-[30rem] overflow-hidden p-8 lg:p-16">
+      <Vortex
+        backgroundColor="transparent"
+        baseHue={120}
+        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+      >
+        <h2 className="text-white text-2xl md:text-5xl font-bold text-center">Open Your Zipphy Account</h2>
+        <p className="text-white text-sm md:text-lg max-w-2xl my-6 text-center">
+          It takes only a few minutes and even fewer clicks to enter the promising and exciting world of trading. Take
+          the first step by clicking on the link below.
+        </p>
+        <button
+          onClick={() => window.open("https://app.zipphy.com/signup", "_blank")}
+          className="flex items-center space-x-1 border border-[#0bf40a33] px-4 py-2 rounded-lg text-white bg-[#0bf40a11] backdrop-blur-lg transition-all duration-300 hover:bg-[#0bf40a44] hover:shadow-[0_0_15px_#0bf40a] hover:border-[#0bf40a]"
+        >
+          <p>Register Now</p>
+          <Icon icon="heroicons:arrow-top-right-on-square" width={16} />
+        </button>
+      </Vortex>
+    </div>
+  );
+}
+
+export default JoinUs;
--- src/pages/investing/account-types/index.jsx
+++ src/pages/investing/account-types/index.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from "react";
+import JoinUs from "./components/JoinUs";
 
 const AccountTypes = () => {
   useEffect(() => {
     document.title = "Zipphy | Investing";
   }, []);
 
-  return <div>Account Types</div>;
+  return (
+    <div className="container mx-auto px-4 py-8 md:px-8 space-y-12">
+      Account Types 3*2 grid
+      <JoinUs />
+    </div>
+  );
 };
 
 export default AccountTypes;
