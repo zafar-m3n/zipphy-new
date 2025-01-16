cafe6c4586a1fecc9669cb156b94337825940b33
zafar-m3n <thezafar.m3n@gmail.com>
2025-01-16 10:59:17 +0530

    Refer a friend

--- src/pages/partnerships/refer-a-friend/index.jsx
+++ src/pages/partnerships/refer-a-friend/index.jsx
@@ -1,7 +1,13 @@
 import React, { useEffect } from "react";
 import images from "@/assets";
+import AnimatedContent from "@/components/animated/AnimatedContent";
+import ShinyText from "@/components/animated/ShinyText";
+import TextGenerateEffect from "@/components/animated/TextGenerate";
+import useWidth from "@/hooks/useWidth";
 
 const ReferFriend = () => {
+  const { width, breakpoints } = useWidth();
+  const isMobile = width < breakpoints.md;
   useEffect(() => {
     document.title = "Zipphy | Partnerships";
   }, []);
@@ -9,65 +15,105 @@ const ReferFriend = () => {
   return (
     <>
       <div className="container mx-auto px-4 py-8 space-y-12">
-        <div className="flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-12">
-          <div className="md:w-1/2">
-            <h2 className="text-3xl md:text-4xl font-bold text-secondary mb-4">Refer a Friend and Earn Rewards</h2>
-            <p className="text-gray-600 md:text-lg">
-              Our referral program is designed to reward both you and your friends. Share your unique referral link, and
-              the more your friends trade, the more you both benefit. Start earning today by inviting your friends to
-              join the platform!
+        <AnimatedContent
+          distance={100}
+          direction="horizontal"
+          reverse={isMobile ? true : false}
+          config={{ tension: 50, friction: 25 }}
+          initialOpacity={0.0}
+          animateOpacity
+          scale={1.0}
+          threshold={0.1}
+        >
+          <div className="flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-12">
+            <div className="md:w-1/2 space-y-4">
+              <ShinyText
+                text="Refer a Friend and Earn Rewards"
+                textColor="#0bf40abb"
+                shineColor="#0bf40a"
+                className="text-2xl md:text-[2rem] font-bold"
+              />
+              <p className="text-gray-300 md:text-lg text-justify">
+                Our referral program is designed to reward both you and your friends. Share your unique referral link,
+                and the more your friends trade, the more you both benefit. Start earning today by inviting your friends
+                to join the platform!
+              </p>
+              <button
+                onClick={() => window.open("https://app.zipphy.com/signup", "_blank")}
+                className="flex items-center space-x-1 border border-[#0bf40a33] px-4 py-2 rounded-lg text-white bg-[#0bf40a11] backdrop-blur-lg transition-all duration-300 hover:bg-[#0bf40a44] hover:shadow-[0_0_15px_#0bf40a] hover:border-[#0bf40a]"
+              >
+                <p>Register Now</p>
+              </button>
+            </div>
+            <div className="md:w-1/2">
+              <img src={images.referFriend} alt="Refer a Friend" className="w-full rounded-lg" />
+            </div>
+          </div>
+        </AnimatedContent>
+        <AnimatedContent
+          distance={100}
+          direction="horizontal"
+          reverse={isMobile ? false : true}
+          config={{ tension: 50, friction: 25 }}
+          initialOpacity={0.0}
+          animateOpacity
+          scale={1.0}
+          threshold={0.1}
+        >
+          <div className="text-center space-y-8">
+            <TextGenerateEffect
+              className="text-center text-2xl md:text-[2rem] font-bold leading-tight tracking-tight text-white"
+              words="Invite Your Friends in 3 Easy Steps"
+              animated={false}
+            />
+            <p className="text-gray-300 md:text-lg">
+              Help your friends get started while earning rewards for yourself. It's a win-win!
             </p>
-            <button className="mt-4 bg-accent text-white py-2 px-6 rounded shadow hover:bg-secondary transition duration-300">
-              Register Now
-            </button>
-          </div>
-          <div className="md:w-1/2">
-            <img src={images.referFriend} alt="Refer a Friend" className="w-full rounded-lg" />
-          </div>
-        </div>
-        <div className="text-center space-y-8">
-          <h2 className="text-3xl md:text-4xl font-bold text-secondary">Invite Your Friends in 3 Easy Steps</h2>
-          <p className="text-gray-600 md:text-lg">
-            Help your friends get started while earning rewards for yourself. It's a win-win!
-          </p>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div className="relative text-center">
-              <div className="absolute inset-0 text-9xl font-bold text-gray-200 -z-10 flex items-center justify-center">
-                1
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <div className="relative text-center">
+                <div className="absolute inset-0 text-9xl font-bold text-gray-200 -z-10 flex items-center justify-center">
+                  1
+                </div>
+                <h3 className="text-xl font-bold text-secondary mt-4 relative">Share Your Unique Link</h3>
+                <p className="text-gray-600 mt-2 relative">
+                  Copy and share your referral link with friends. You can find your unique link in your account
+                  dashboard.
+                </p>
               </div>
-              <h3 className="text-xl font-bold text-secondary mt-4 relative">Share Your Unique Link</h3>
-              <p className="text-gray-600 mt-2 relative">
-                Copy and share your referral link with friends. You can find your unique link in your account dashboard.
-              </p>
-            </div>
 
-            <div className="relative text-center">
-              <div className="absolute inset-0 text-9xl font-bold text-gray-200 -z-10 flex items-center justify-center">
-                2
+              <div className="relative text-center">
+                <div className="absolute inset-0 text-9xl font-bold text-gray-200 -z-10 flex items-center justify-center">
+                  2
+                </div>
+                <h3 className="text-xl font-bold text-secondary mt-4 relative">Friend Signs Up</h3>
+                <p className="text-gray-600 mt-2 relative">
+                  Once your friend clicks on the link, they’ll be guided through the sign-up process and can start
+                  trading right away.
+                </p>
               </div>
-              <h3 className="text-xl font-bold text-secondary mt-4 relative">Friend Signs Up</h3>
-              <p className="text-gray-600 mt-2 relative">
-                Once your friend clicks on the link, they’ll be guided through the sign-up process and can start trading
-                right away.
-              </p>
-            </div>
 
-            {/* Step 3 */}
-            <div className="relative text-center">
-              <div className="absolute inset-0 text-9xl font-bold text-gray-200 -z-10 flex items-center justify-center">
-                3
+              {/* Step 3 */}
+              <div className="relative text-center">
+                <div className="absolute inset-0 text-9xl font-bold text-gray-200 -z-10 flex items-center justify-center">
+                  3
+                </div>
+                <h3 className="text-xl font-bold text-secondary mt-4 relative">Earn Exclusive Rewards</h3>
+                <p className="text-gray-600 mt-2 relative">
+                  When your friend starts trading, both of you will earn rewards. Enjoy perks such as bonuses,
+                  discounts, or other exclusive benefits.
+                </p>
               </div>
-              <h3 className="text-xl font-bold text-secondary mt-4 relative">Earn Exclusive Rewards</h3>
-              <p className="text-gray-600 mt-2 relative">
-                When your friend starts trading, both of you will earn rewards. Enjoy perks such as bonuses, discounts,
-                or other exclusive benefits.
-              </p>
+            </div>
+            <div className="flex justify-center items-center">
+              <button
+                onClick={() => window.open("https://app.zipphy.com/signup", "_blank")}
+                className="self-center flex items-center space-x-1 border border-[#0bf40a33] px-4 py-2 rounded-lg text-white bg-[#0bf40a11] backdrop-blur-lg transition-all duration-300 hover:bg-[#0bf40a44] hover:shadow-[0_0_15px_#0bf40a] hover:border-[#0bf40a]"
+              >
+                <p>Register Now</p>
+              </button>
             </div>
           </div>
-          <button className="mt-8 bg-accent text-white py-2 px-6 rounded shadow hover:bg-secondary transition duration-300">
-            Register Now
-          </button>
-        </div>
+        </AnimatedContent>
       </div>
     </>
   );
